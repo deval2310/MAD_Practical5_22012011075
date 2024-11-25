a67235377650bf6d2b2ec64196132fab8c9bd538
Deval Patel <121814905+deval2310@users.noreply.github.com>
2024-11-25 14:57:08 +0530

    All Done

---
+++ app/src/main/java/com/example/mad_practical5_22012011075/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mad_practical5_22012011075
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mad_practical5_22012011075.ui.theme.AppNavigation
+import com.example.mad_practical5_22012011075.ui.theme.MAD_Practical5_22012011075Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppNavigation()
+        }
+    }
+}
